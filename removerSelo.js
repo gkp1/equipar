70ff10378faec64481395e4caf0fbaa257cfdd37
gkp1 <63517877+gkp1@users.noreply.github.com>
2024-02-29 18:36:44 -0300

    place wa on top bar on all pages.

--- removerSelo.js
+++ removerSelo.js
@@ -34,7 +34,14 @@ function replaceText(dir) {
     } else {
       let fileContent = fs.readFileSync(filePath, "utf8");
       count++;
-      if (fileContent.includes(`<div class="social">`)) {
+      if (fileContent.includes(`<div class="social">
+            <a
+              rel="nofollow"
+              class="fright"
+              title="Clique e ligue"
+              href="https://wa.me/5517997724429?text=Bom%20dia.%20Vim%20do%20website%20e%20gostaria%20de%20algumas%20informa%C3%A7%C3%B5es"
+              ><image src="imagens/icones/wa-icon-15px-white.png"></image> 17 <strong>99772-4429</strong></a
+            >`)) {
         fileContent = fileContent.replace(
           `<div class="social">`,
           `<div class="social">
